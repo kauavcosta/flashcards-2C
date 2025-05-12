e6e72c7959f72b0c7a1a58f3cd0a82d4b323cd14
Kauã Vinicius <costa.rosa.kaua@escola.pr.gov.br>
2025-05-12 14:46:06 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'Linguagem de programação'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se escreve dia do paagmento?',
+    'Pay Day'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o gráfico da função quadrática?',
+    'Parábola'
+)
