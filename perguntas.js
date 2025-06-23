e7eac7ab21b3695b0c2fc777b6f8ec98a02959ad
Kauã Vinicius <costa.rosa.kaua@escola.pr.gov.br>
2025-06-23 11:49:36 +0000

    ok

--- perguntas.js
+++ perguntas.js
@@ -5,13 +5,29 @@ criaCartao(
 )
 
 criaCartao(
-    'Inglês',
-    'Como se escreve dia do pagamento?',
-    'Pay Day'
+    'Geografia',
+    'Qual o menor estado do Brasil?',
+    'Sergipe'
 )
 
 criaCartao(
     'Matemática',
     'Qual o gráfico da função quadrática?',
     'Parábola'
+)
+
+criaCartao(
+    'Química',
+'O que significa NOX?',
+'É o número de oxidação' 
+)
+criaCartao(
+'Geografia' ,
+'Qual o maior planeta do sistema solar?',
+'Júpiter'
+)
+criaCartao(
+'Artes',
+'Quem pintou Guernica?',
+'Pablo Picasso'
 )
